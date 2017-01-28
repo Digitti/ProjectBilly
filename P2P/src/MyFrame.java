f9b5a4e044454ecc7b364de82203b443d26c2cca
KeviN <kevin.adagba@gmail.com>
2017-01-28 23:06:54 +0100

    definition des trames sous la forme de structure

--- P2P/src/MyFrame.java
+++ P2P/src/MyFrame.java
@@ -1,4 +1,23 @@
 
 public interface MyFrame {
 	
+	class frameUdpRequest
+	{
+		public byte lenght;		// taille du fichier
+		public byte ipType;		// type d'adresse IPV4 ou IPV6
+		public byte[] addr = new byte[16];		// addresse IP
+		public short port;		//numero du port
+		public byte requestType;		// type de requete par nom ou racine
+		public byte[] nameOrHash = new byte[16];	// hash ou nom du fichier demander
+	}
+	
+	class frameUdpResponse
+	{
+		public byte requestType;		// type de requete par nom ou racine
+		public byte[] nameOrHash = new byte[16];		// hash ou nom du fichier demander
+		//Reponse
+		public int nbrFile;		// nombre de fichier qui constitue le fichier
+		public byte[] racineHash = new byte[16];		// hash du fichier racine
+		public byte nameFile;		// nom du fichier
+	}
 }
